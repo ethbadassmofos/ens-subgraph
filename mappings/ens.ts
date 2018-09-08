7b486de73c1929c60f25ea566ddd506313c7f8ff
Ramesh Nair <ram@hiddentao.com>
2018-09-08 12:39:25 +0200

    fix mapping

--- mappings/ens.ts
+++ mappings/ens.ts
@@ -1,12 +1,12 @@
 export function domainRegistered(event: NewOwner): void {
   let node = event.params.node.toHex()
   let label = event.params.node.toHex()
-  // let address = event.params.address
+  let owner = event.params.owner
 
   let registration = new Entity()
   registration.setString('node', node)
   registration.setString('label', label)
-  // registration.setString('owner', `${address}`)
+  registration.setAddress('owner', owner)
 
-  store.set('Registration', `${node}-${label}`, registration)
+  store.set('Registration', node + '-' + label, registration)
 }
